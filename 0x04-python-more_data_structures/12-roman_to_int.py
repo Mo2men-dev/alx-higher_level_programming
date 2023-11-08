27fa84ac2439180a3948557f7af263b0ac20d6ca
Momen Sameh <momensameh5@gmail.com>
2023-11-08 12:06:08 -0800

    edited 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
+    if type(roman_string) != 'str' or roman_string == None:
+        return 0
     roman_dic = {
             'I': 1,
             'V': 5,
