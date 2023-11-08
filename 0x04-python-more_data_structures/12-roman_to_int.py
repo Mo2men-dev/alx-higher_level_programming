094675f6c93c92cb067cfbb6866b1efe937aecf6
Momen Sameh <momensameh5@gmail.com>
2023-11-08 11:53:11 -0800

    edited 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -17,10 +17,18 @@ def roman_to_int(roman_string):
         if i != len(roman_string) - 1:
             if crr_n < roman_dic[roman_string[i + 1]]:
                 lst.append(roman_dic[roman_string[i + 1]] - crr_n)
-                i += 2
+                i += 1
             else:
                 lst.append(crr_n)
         else:
             lst.append(crr_n)
         i += 1
     return sum(lst)
+
+print(roman_to_int("III"))      # Output: 3
+print(roman_to_int("IX"))        # Output: 9
+print(roman_to_int("LVIII"))     # Output: 58
+print(roman_to_int("CDXLIV"))    # Output: 444
+print(roman_to_int("CMXCIX"))    # Output: 999
+print(roman_to_int("MMMCMXCIX")) # Output: 3999
+
