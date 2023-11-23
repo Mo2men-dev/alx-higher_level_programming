127ca4cc6327ca63762f321122199c76e5acb201
Momen Sameh <momensameh5@gmail.com>
2023-11-22 17:26:56 -0800

    fixing docmunetation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python3
-
 class Square:
-    """Empty class that defines a square"""
-    pass
+    """
+    This is a simple class that defines a square.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+    """
+
+    def __init__(self):
+        """
+        This is the constructor method for the Square class.
+
+        Parameters:
+            None
+
+        Returns:
+            None
+        """
+        pass
+
