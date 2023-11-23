e5f0119b2c27928b3383088631e9ec492f2ecb6d
Momen Sameh <momensameh5@gmail.com>
2023-11-22 17:23:13 -0800

    added files for the project

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+class Square:
+    '''Defines a Square'''
+    def __init__(self, size):
+        '''Init the data'''
+        self.__size = size
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+README for the 0x06-python-classes project
