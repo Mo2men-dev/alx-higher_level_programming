abd68db279fc71cfa3b33b219bd14e1399af08de
Momen Sameh <momensameh5@gmail.com>
2023-11-30 16:04:55 -0800

    4th file

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -57,3 +57,8 @@ class Rectangle:
         if (self.__width == 0 or self.__height == 0):
             return (0)
         return (2 * (self.__width + self.__height))
+
+    def __str__(self):
+        if (self.__width == 0 or self.__height == 0):
+            return ("")
+        return (("#" * self.__width + "\n") * self.__height)[:-1]
