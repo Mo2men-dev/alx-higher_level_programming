65503bd62ce53e3ac7f3bf9af6d0a5f5ca03cc3c
Momen Sameh <momensameh5@gmail.com>
2023-12-09 14:18:29 -0800

    added few files

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""Module that defines a square class"""
+"""Square class Module"""
+Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
     """Square class"""
-
     def __init__(self, size):
-        """__init__ method that initializes size"""
+        """Method for initializing a square"""
         self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
-        """area method that returns area of Square"""
+        """Method that returns area of a square"""
         return self.__size ** 2
