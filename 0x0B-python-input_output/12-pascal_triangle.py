8dbe85215e5d5a43904fd7cd0383d03e38191a5c
Momen Sameh <momensameh5@gmail.com>
2024-02-08 07:52:15 +0000

    added 12-pascal_triangle.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""pascal triangle using list of lists module"""
+
+
+def pascal_tri(n):
+    """
+    prints the pascal triangle
+
+    Args:
+        n (int): number of rows to be printed
+
+    Returns:
+        None
+    """
+    o = [[1], [1, 1]]
+    for i in range(1, n - 1):
+        temp = []
+        temp.append(1)
+
+        for j in range(len(o[i]) - 1):
+            temp.append(o[i][j] + o[i][j + 1])
+
+        temp.append(1)
+        o.append(temp)
+
+    for i in range(len(o)):
+        print(o[i])
