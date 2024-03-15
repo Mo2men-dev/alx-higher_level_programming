4b2e7466711bfed4372daf70f89e183fad59c61e
Momen Sameh <momensameh5@gmail.com>
2024-03-15 07:38:29 -0700

    create a superuser

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,7 +3,5 @@ CREATE USER
     IF NOT EXISTS 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
-   ON test_database.*
-   TO 'user_0d_1'@'localhost'
-   IDENTIFIED BY 'user_0d_1_pwd';
-FLUSH PRIVILEGES;
+   ON *.*
+   TO 'user_0d_1'@'localhost';
