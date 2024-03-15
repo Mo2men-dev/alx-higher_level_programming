b4b09729f358a759fcbf8319c83b43a20906bfa6
Momen Sameh <momensameh5@gmail.com>
2024-03-15 08:26:05 -0700

    L

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- lists all cities contained in the database
 SELECT cities.`id`, cities.`name`, states.`name`
-FROM cities INNER JOIN states ON cities.`state_id` = state.`id`
-ORDER BY c.`id`;
+FROM `cities` INNER JOIN `states` ON cities.`state_id` = state.`id`
+ORDER BY cities.`id`;
