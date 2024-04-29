6d404a7da19ecf2648039e433159c7eb2b2d2374
Momen Sameh <momensameh5@gmail.com>
2024-04-29 00:34:57 -0700

    args

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+
+if (argv.length === 3) {
+  console.log('Argument found');
+}
+
+else if (argv.lenght > 3) {
+  console.log('Arguments found');
+}
+
+else {
+  console.log('No argument');
+}
