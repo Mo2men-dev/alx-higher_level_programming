858055b8035849decc6fe8d74bfb68bafc4a9ff7
Momen Sameh <momensameh5@gmail.com>
2024-05-14 04:00:27 -0700

    secound task

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+usr, pw, name = sys.argv[1], sys.argv[2], sys.argv[3]
+
+try:
+    db = MySQLdb.connect('localhost', usr, pw, name)
+except Exception as err:
+    raise err("couldn't connect to db")
+
+c = db.cursor()
+c.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id")
+arr = c.fetchall()
+for e in arr:
+    print(e)
