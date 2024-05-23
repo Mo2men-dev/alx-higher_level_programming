60b9121cc10fe74f21fa9bb4fdec0a3b285cf61b
Momen Sameh <momensameh5@gmail.com>
2024-05-23 10:30:53 -0700

    we back?

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,18 +7,27 @@ import sys
 import MySQLdb
 
 
-args = sys.argv
-usr, pw, name = args[1], args[2], args[3]
-state_name = args[4]
+def connect_db:
+    """
+    connects to the db
+    """
+    args = sys.argv
+    usr, pw, name = args[1], args[2], args[3]
+    state_name = args[4]
 
-try:
-    db = MySQLdb.connect('localhost', usr, pw, name)
+    try:
+        db = MySQLdb.connect('localhost', usr, pw, name)
 
-except Exception as e:
-    raise("couldn't conncet to db")
+    except Exception as e:
+        raise("couldn't conncet to db")
 
-c = db.cursor()
-c.execute("SELECT * FROM states WHERE name = \'{}\' ORDER BY state.id".format(state_name))
-arr = c.fetchall()
-for e in arr:
-    print(e)
+    c = db.cursor()
+    c.execute('''SELECT * FROM states WHERE name = \'{}\'
+            ORDER BY state.id'''.format(state_name))
+    arr = c.fetchall()
+    for e in arr:
+        print(e)
+
+
+if __name__ == "__main__":
+    conncet_db()
