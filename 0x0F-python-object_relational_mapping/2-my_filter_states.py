8950893ea2e44bdc38c7633205ebada28d6cb1aa
Momen Sameh <momensameh5@gmail.com>
2024-05-23 10:14:53 -0700

    we back?

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""displays values from args
+"""
+
+import sys
+import MySQLdb
+
+
+args = sys.argv
+usr, pw, name = args[1], args[2], args[3]
+state_name = args[4]
+
+try:
+    db = MySQLdb.connect('localhost', usr, pw, name)
+
+except Exception as e:
+    raise("couldn't conncet to db")
+
+c = db.cursor()
+c.execute('SELECT * FROM states WHERE name = %s', state_name)
+arr = c.fetchall()
+for e in arr:
+    print(e)
