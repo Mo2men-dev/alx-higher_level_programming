ae15a5e7a730eb77a76d762ed301442f2cb23c0f
Momen Sameh <momensameh5@gmail.com>
2024-05-23 11:01:33 -0700

    iam lossing my mind :)

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""list all cities from a database"""
+
+import sys
+import MySQLdb
+
+
+def sql_connect():
+    """connect to sql server
+    """
+    try:
+        connection = MySQLdb.connect(
+                'localhost', sys.argv[1], sys.argv[2], sys.argv[3])
+    except Exception as err:
+        raise err("can't connect to sql server")
+        return 0
+    curse = connection.cursor()
+    curse.execute('SELECT * FROM cities ORDER BY cities.id')
+    arr = curse.fetchall()
+    for data in arr:
+        print(data)
+
+
+if __name__ == '__main__':
+    sql_connect()
