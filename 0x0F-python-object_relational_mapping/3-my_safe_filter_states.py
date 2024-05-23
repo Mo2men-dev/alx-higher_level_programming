fa49a87a75ac6a99e61635d80aa828c5d7e470cf
Momen Sameh <momensameh5@gmail.com>
2024-05-23 10:56:34 -0700

    iam lossing my mind :)

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""displays the states based on args
+"""
+
+import sys
+import MySQLdb
+
+
+def connect_db():
+    """connects to the db
+    """
+    args = sys.argv
+    usr, pw, name = args[1], args[2], args[3]
+    state_name = args[4]
+
+    try:
+        db = MySQLdb.connect('localhost', usr, pw, name)
+
+    except Exception as e:
+        raise e("couldn't conncet to db")
+
+    c = db.cursor()
+    c.execute('SELECT * FROM states')
+    arr = c.fetchall()
+    for e in arr:
+        if e == state_name:
+            print(e)
+
+
+if __name__ == "__main__":
+    connect_db()
