2c57a456d93f4a08bc35a93f517a7a82b1895a95
Momen Sameh <momensameh5@gmail.com>
2024-05-24 07:37:45 -0700

    lesgoooo

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""get cities based on state
+"""
+
+import sys
+import MySQLdb
+
+
+def connect_db():
+    """connects to the db
+    """
+    args = sys.argv
+    usr, pw, name = args[1], args[2], args[3]
+    state_name = args[4]
+    try:
+        db = MySQLdb.connect('localhost', usr, pw, name)
+    except Exception as e:
+        raise e("couldn't connect to db")
+
+    c = db.cursor()
+    c.execute("""
+            SELECT cities.name
+            FROM cities INNER JOIN states
+            ON cities.state_id = states.id
+            WHERE states.id = (SELECT states.id FROM states
+            WHERE states.name = \'{}\')""".format(state_name))
+
+    arr = c.fetchall()
+    for i, e in enumerate(arr):
+        if i != len(arr) - 1:
+            print(e[0], end=", ")
+        else:
+            print(e[0])
+
+
+if __name__ == "__main__":
+    connect_db()
