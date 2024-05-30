2ac861848bb2641418bc5756e65bf0f74b1e0c4d
Momen Sameh <momensameh5@gmail.com>
2024-05-30 13:36:07 -0700

    finally understanad SQLA (i think)

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,5 +12,5 @@ class State(Base):
     """state class for the states table"""
     __tablename__ = "states"
     id = Column(Integer, primary_key=True, nullable=False,
-            autoincrement=True, unique=True)
+                autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
