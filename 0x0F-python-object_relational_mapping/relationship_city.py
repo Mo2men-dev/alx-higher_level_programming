c46923bc3dce4d88bfadcc4b4d5596d9a443a5ea
Momen Sameh <momensameh5@gmail.com>
2024-05-30 19:04:19 -0700

    done?

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""module for the cities model
-"""
+"""Lists states"""
 
-from relationship_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
-
+from relationship_state import Base
 
 class City(Base):
-    """city class for citis table
-    """
+    """Class representing the states table"""
+    __tablename__ = 'cities'
 
-    __tablename__ = "cities"
-    id = Column(Integer, autoincrement=True, unique=True,
-                nullable=False, primary_key=True)
+    id = Column(Integer, nullable=False, primary_key=True,
+                autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""SQL Alchemy states model
-"""
+"""Lists states"""
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -10,11 +9,13 @@ Base = declarative_base()
 
 
 class State(Base):
-    """state class for the states table"""
-    __tablename__ = "states"
-    id = Column(Integer, primary_key=True, nullable=False,
+    """Class representing the states table"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, nullable=False, primary_key=True,
                 autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
+
     cities = relationship(
         "City",
         cascade="all, delete-orphan",
