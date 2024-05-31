520a1432eb4fdf872829fbdc4312b3a1feb74303
Momen Sameh <momensameh5@gmail.com>
2024-05-30 18:44:20 -0700

    done?

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""module for the cities model
+"""
+
+from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+class City(Base):
+    """city class for citis table
+    """
+
+    __tablename__ = "cities"
+    id = Column(Integer, autoincrement=True, unique=True,
+                nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
